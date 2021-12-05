e83b8f7909a1d6af69bef443bd101ef153e21415
Aileen Rae <aileenlrae@gmail.com>
2021-12-05 21:26:09 +0000

    Day 01; part 2

--- day-01/index.js
+++ day-01/index.js
@@ -14,15 +14,37 @@ const countDepthMeasurementIncreases = measurements => {
   }, 0);
 };
 
+const countSlidingWindowIncreases = (measurements, window = 3) => {
+  let prevSum;
+  return measurements.reduce((acc, curr, idx) => {
+    const currSum = measurements
+      .slice(idx, window + idx)
+      .reduce((a, c) => a + c);
+    if (prevSum && currSum > prevSum) {
+      acc++;
+    }
+    prevSum = currSum;
+    return acc;
+  }, 0);
+};
+
 const run = () => {
+  // Part 1
   const filepath = './data/challenge.txt';
   const measurements = txtFileLinesToArray(filepath).map(line =>
     parseInt(line, 10)
   );
   const increaseCount = countDepthMeasurementIncreases(measurements);
+  console.log('Part 1 answer:');
   console.log({ increaseCount });
+
+  // Part 2
+  const slidingWindowIncreaseCount = countSlidingWindowIncreases(measurements);
+  console.log('\n');
+  console.log('Part 2 answer:');
+  console.log({ slidingWindowIncreaseCount });
 };
 
 run();
 
-export { countDepthMeasurementIncreases };
+export { countDepthMeasurementIncreases, countSlidingWindowIncreases };
--- day-01/index.test.js
+++ day-01/index.test.js
@@ -1,8 +1,16 @@
-import { countDepthMeasurementIncreases } from './index.js';
+import {
+  countDepthMeasurementIncreases,
+  countSlidingWindowIncreases,
+} from './index.js';
 
 const testData = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
 
-it("calculates correct no. increases from example data", () => {
+it('Part 1: calculates correct no. increases from example data', () => {
   const increaseCount = countDepthMeasurementIncreases(testData);
   expect(increaseCount).toEqual(7);
+});
+
+it('Part 2: calculates correct sliding window no. increases from example data', () => {
+  const slidingWindowIncreaseCount = countSlidingWindowIncreases(testData);
+  expect(slidingWindowIncreaseCount).toEqual(5);
 });
